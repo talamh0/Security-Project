887c45f8f3d342a16cf3fc7b89cb135d9bf07c20
tala <herewecode04@gmail.com>
2025-12-07 19:44:31 +0300

    Update index.php

--- Secure Site/User/index.php
+++ Secure Site/User/index.php
@@ -1,17 +1,7 @@
 
 <?php
-// FORCE HTTPS
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect");
-    exit();
-}
-
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_samesite', 1);
-session_start();
-
+// Load security configurations (HTTPS enforcement, session setup) and start session.
+require_once 'security_config.php';
 
 include 'config.php';
 
