7e102dab81e91f7e372af6b1cee53bbf14fa3055
tala <herewecode04@gmail.com>
2025-12-07 18:43:29 +0300

    Enhance session cookie security settings
    Added security settings for session cookies.

--- Secure Site/Admin/manageEvents.php
+++ Secure Site/Admin/manageEvents.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+ini_set('session.cookie_samesite', 1);
 session_start();
 
 // Restrict access to admin users only
