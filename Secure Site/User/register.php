a2346b09ab95766912c7662baa90e00f3cfe8205
tala <herewecode04@gmail.com>
2025-12-07 19:22:31 +0300

    Implement HTTPS redirection in register.php
    Added HTTPS redirection to ensure secure connections.

--- Secure Site/User/register.php
+++ Secure Site/User/register.php
@@ -1,4 +1,11 @@
 <?php
+// FORCE HTTPS
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $redirect");
+    exit();
+}
+
 include 'config.php';
 
 // store errors
