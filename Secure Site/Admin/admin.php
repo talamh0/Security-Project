a6bfa9908f33ca45577da9bf95995ecafea48b50
tala <herewecode04@gmail.com>
2025-12-07 19:19:57 +0300

    Update admin.php

--- Secure Site/Admin/admin.php
+++ Secure Site/Admin/admin.php
@@ -1,4 +1,11 @@
 <?php
+// FORCE HTTPS
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $redirect");
+    exit();
+}
+
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_samesite', Strict);
