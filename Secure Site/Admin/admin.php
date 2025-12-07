377a20627e456634fc466773c11767e628a13d04
tala <herewecode04@gmail.com>
2025-12-07 19:29:24 +0300

    Refactor admin.php to include security config
    Removed HTTPS enforcement code and added security configuration requirement.

--- Secure Site/Admin/admin.php
+++ Secure Site/Admin/admin.php
@@ -1,15 +1,6 @@
 <?php
-// FORCE HTTPS
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect");
-    exit();
-}
 
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_samesite', Strict);
-session_start();
+require_once 'security_config.php';
 
 if(isset($_SESSION['admin_logged_in'])){
     header("Location: manageEvents.php");
