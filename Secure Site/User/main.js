96ccaa7e4a7c31f960a6fdbfd7e1708e786db60e
tala <herewecode04@gmail.com>
2025-12-07 20:33:11 +0300

    Remove commented Arabic text from main.js

--- Secure Site/User/main.js
+++ Secure Site/User/main.js
@@ -6,7 +6,7 @@
         const ratingInput = document.getElementById("review-rating");
         const ratingStars = document.querySelectorAll("#rating span");
 
-        // جلب التعليقات من السيرفر
+       
         fetch("get-reviews.php")
         .then(res => res.json())
         .then(data => {
@@ -14,7 +14,7 @@
             data.forEach(review => addReviewToList(review, false));
         });
 
-        // تفعيل النجوم التفاعلية
+        
         ratingStars.forEach(star => {
             star.addEventListener("mouseover", () => highlightStars(star.dataset.value));
             star.addEventListener("mouseout", () => highlightStars(ratingInput.value));
@@ -30,7 +30,7 @@
             });
         }
 
-        // إضافة تعليق جديد للسيرفر والواجهة
+        
         reviewForm.addEventListener("submit", e => {
             e.preventDefault();
             const text = document.getElementById("review-input").value.trim();
@@ -55,7 +55,7 @@
             }
         });
 
-        // دالة لإضافة تعليق للقائمة
+        
         function addReviewToList(review, prepend = true) {
             const reviewItem = document.createElement("div");
             reviewItem.classList.add("review-item");
